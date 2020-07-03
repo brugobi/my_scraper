137afb968416c9cd07e4fdd18cf664a124de3bcd
brugobi <brgobbi@hotmail.com>
2020-07-03 17:22:05 -0400

    get the information from indeed: job_title, company_name and data

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,39 @@
-require 'HTTParty'
+# require 'HTTParty'
 require 'Nokogiri'
+require 'open-uri'
 
-class Scraper
-    attr_accessor :parse_page
+# class Scraper
+#     attr_accessor :parse_page
 
-    def initialize
-        doc = HTTParty.get("https://ca.indeed.com/jobs?q=developer&l=London%2C+ON")
-        @parse_page ||= Nokogiri::HTML(doc) #merorized the @parse_page so  it only gets assigned once
-    end    
+#     def initialize
+#         doc = HTTParty.get("https://ca.indeed.com/jobs?q=developer&l=London%2C+ON")
+#         @parse_page ||= Nokogiri::HTML(doc) #merorized the @parse_page so  it only gets assigned once
+#     end 
+    
+#     titles = parse_page.css('.jobsearch-SerpJobCard').css('.title').css('.jobtitle').childrem.map { |title| title.text }.compact
+# end
+
+url = "https://ca.indeed.com/jobs?q=developer&l=London%2C+ON"
+document = open(url)
+content = document.read
+parsed_content = Nokogiri::HTML(content)
+
+parsed_content.css('.jobsearch-SerpJobCard').css('.title').each do |jobtitle|
+    title = jobtitle.css('.jobtitle').inner_text
+    link =  jobtitle.css('.jobtitle').first.attributes["href"].value
+    puts title
+    # puts link
+    puts '------------------------------'
 end
+
+# parsed_content.css('.jobsearch-SerpJobCard').css('.sjcl').each do |company_name|
+#     name = company_name.css('.company').inner_text
+#     puts name
+#     puts '------------------------------'
+# end
+
+# parsed_content.css('.jobsearch-SerpJobCard').css('.jobsearch-SerpJobCard-footer').css('.jobsearch-SerpJobCard-footerActions').css(('result-link-bar-container').css('.result-link-bar').each do |date|
+#     date_info = date.css('.date').inner_text
+#     puts date_info
+#     puts '------------------------------'
+# end
