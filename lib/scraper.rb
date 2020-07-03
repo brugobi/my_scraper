561197ebf69ad4f39fe28ce72c188dd24dfdca12
brugobi <brgobbi@hotmail.com>
2020-07-03 18:17:33 -0400

    add methods for each info, get the info using httparty gem and refactoring the code

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,22 +1,46 @@
-# require 'HTTParty'
+require 'httparty'
 require 'Nokogiri'
-require 'open-uri'
+#require 'open-uri'
 
-# class Scraper
-#     attr_accessor :parse_page
+class Scraper
+    attr_accessor :parse_page
 
-#     def initialize
-#         doc = HTTParty.get("https://ca.indeed.com/jobs?q=developer&l=London%2C+ON")
-#         @parse_page ||= Nokogiri::HTML(doc) #merorized the @parse_page so  it only gets assigned once
-#     end 
+    def initialize
+        doc = HTTParty.get("https://ca.indeed.com/jobs?q=developer&l=London%2C+ON")
+        @parse_page ||= Nokogiri::HTML(doc) #merorized the @parse_page so  it only gets assigned once
+    end 
+
+    def jobsearch_container
+        parse_page.css('.jobsearch-SerpJobCard')
+    end
     
-#     titles = parse_page.css('.jobsearch-SerpJobCard').css('.title').css('.jobtitle').childrem.map { |title| title.text }.compact
-# end
+    def get_title
+        jobsearch_container.css('.title').css('.jobtitle').children.map { |title| title.text }.compact
+    end
+    
+    def get_company
+        jobsearch_container.css('.sjcl').css('.company').children.map { |title| title.text }.compact
+    end
+    
+    def get_date
+        parse_page.css('.result-link-bar-container').css('.result-link-bar').css('.date').children.map { |title| title.text }.compact
+    end
+    
+    scraper = Scraper.new
+    titles = scraper.get_title
+    names = scraper.get_company
+    dates = scraper.get_date
 
-url = "https://ca.indeed.com/jobs?q=developer&l=London%2C+ON"
-document = open(url)
-content = document.read
-parsed_content = Nokogiri::HTML(content)
+    (0...names.size).each do |index|
+        puts "--- index: #{index + 1} ---"
+        puts "Job Title: #{titles[index]} | Company: #{names[index]} | Date: #{dates[index]}"
+    end    
+end
+
+# url = "https://ca.indeed.com/jobs?q=developer&l=London%2C+ON"
+# document = open(url)
+# content = document.read
+# parsed_content = Nokogiri::HTML(content)
 
 # parsed_content.css('.jobsearch-SerpJobCard').css('.title').each do |jobtitle|
 #     title = jobtitle.css('.jobtitle').inner_text
@@ -32,4 +56,4 @@ parsed_content = Nokogiri::HTML(content)
 #     puts '------------------------------'
 # end
 
-puts parsed_content.css('.result-link-bar-container').css('.result-link-bar').css('.date').first.inner_text
+# puts parsed_content.css('.result-link-bar-container').css('.result-link-bar').css('.date').first.inner_text
