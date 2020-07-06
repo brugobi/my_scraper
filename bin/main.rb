92fabc5ece95bbf901129fa369fdfb201d3fc12f
brugobi <brgobbi@hotmail.com>
2020-07-06 17:30:42 -0400

    add get_link file with a get_link class

--- bin/main.rb
+++ bin/main.rb
@@ -4,8 +4,16 @@
     titles = scraper.get_title
     names = scraper.get_company
     dates = scraper.get_date
+    # links = scraper.get_link
 
     (0...names.size).each do |index|
-        puts "--- index: #{index + 1} ---"
-        puts "Job Title: #{titles[index]} | Company: #{names[index]} | Date: #{dates[index]}"
+        puts "-------------------- index: #{index + 1} --------------------"
+        puts "Job Title: #{titles[index]}"
+        # puts "Link: #{links[index]}"
+        puts "Company: #{names[index]}"
+        puts "Date: #{dates[index]}"
     end
+
+    # puts titles.class
+    # puts names.class
+    # puts dates.class
