476385c2b02ef0f900f1a9eb268d3caa3546759b
brugobi <brgobbi@hotmail.com>
2020-07-06 11:16:10 -0400

    add scraper_spec.rb file

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,10 +9,6 @@ class Scraper
         doc = HTTParty.get("https://ca.indeed.com/jobs?q=developer&l=London%2C+ON")
         @parse_page ||= Nokogiri::HTML(doc) #merorized the @parse_page so  it only gets assigned once
     end 
-
-    def jobsearch_container
-        parse_page.css('.jobsearch-SerpJobCard')
-    end
     
     def get_title
         jobsearch_container.css('.title').css('.jobtitle').children.map { |title| title.text }.compact
@@ -25,16 +21,12 @@ class Scraper
     def get_date
         parse_page.css('.result-link-bar-container').css('.result-link-bar').css('.date').children.map { |title| title.text }.compact
     end
-    
-    scraper = Scraper.new
-    titles = scraper.get_title
-    names = scraper.get_company
-    dates = scraper.get_date
 
-    (0...names.size).each do |index|
-        puts "--- index: #{index + 1} ---"
-        puts "Job Title: #{titles[index]} | Company: #{names[index]} | Date: #{dates[index]}"
-    end    
+    private 
+    
+    def jobsearch_container
+        parse_page.css('.jobsearch-SerpJobCard')
+    end
 end
 
 # url = "https://ca.indeed.com/jobs?q=developer&l=London%2C+ON"
---
+++ spec/scraper_spec.rb
@@ -0,0 +1,3 @@
+#spec/scraper_spec.rb
+require_relative '../bin/main.rb'
+require_relative '../lib/scraper.rb'
