2d882ab5fa494222acacadaf94abba5528feb95b
brugobi <brgobbi@hotmail.com>
2020-07-07 12:43:15 -0400

    deleted variables class and comments

--- bin/main.rb
+++ bin/main.rb
@@ -4,16 +4,10 @@ require_relative '../lib/variables.rb'
     titles = scraper.get_title
     names = scraper.get_company
     dates = scraper.get_date
-    #links = scraper.get_link
 
     (0...names.size).each do |index|
         puts "-------------------- index: #{index + 1} --------------------"
         puts "Job Title: #{titles[index]}"
-        # puts "Link: #{links[index]}"
         puts "Company: #{names[index]}"
         puts "Date: #{dates[index]}"
     end
-
-    # puts titles.class
-    # puts names.class
-    # puts dates.class
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,18 +10,7 @@ class Scraper
         page = document.read
         @parse_page = Nokogiri::HTML(page) 
     end 
-
-    # def initialize (link)
-    #     @title = link.jobsearch_container.css('.title').css('.jobtitle').children.map { |title| title.text }.compact
-    #     @company = link.jobsearch_container.css('.sjcl').css('.company').children.map { |title| title.text }.compact
-    #     @date = link.css('.result-link-bar-container').css('.result-link-bar').css('.date').children.map { |title| title.text }.compact
-    # end
-
-    # def get_title (title)
-    #     return false if title.empty?
-    #     title
-    # end    
-    
+        
     def get_title
         title = jobsearch_container.css('.title').css('.jobtitle').children.map { |title| title.text }
         return false if title.empty?
@@ -45,30 +34,5 @@ class Scraper
     def jobsearch_container
         parse_page.css('.jobsearch-SerpJobCard')
     end
-
-    # def get_link
-    #     jobsearch_container.css('.title').css('.jobtitle').first.attributes["href"].value
-    # end 
     
 end
-
-# url = "https://ca.indeed.com/jobs?q=developer&l=London%2C+ON"
-# document = open(url)
-# content = document.read
-# parsed_content = Nokogiri::HTML(content)
-
-# parsed_content.css('.jobsearch-SerpJobCard').css('.title').each do |jobtitle|
-#     title = jobtitle.css('.jobtitle').inner_text
-#     link =  "https://ca.indeed.com#{jobtitle.css('.jobtitle').first.attributes['href'].value}"
-#     puts title
-#     puts link
-#     puts '------------------------------'
-# end
-
-# parsed_content.css('.jobsearch-SerpJobCard').css('.sjcl').each do |company_name|
-#     name = company_name.css('.company').inner_text
-#     puts name
-#     puts '------------------------------'
-# end
-
-# puts parsed_content.css('.result-link-bar-container').css('.result-link-bar').css('.date').first.inner_text
